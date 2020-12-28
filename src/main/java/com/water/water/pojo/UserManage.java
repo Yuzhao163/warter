4d46ee5b7ad6843a6666669ea66cc8e9fa0e18ac
aotexiaowaibadan <72897782+aotexiaowaibadan@users.noreply.github.com>
2020-12-28 10:13:09 +0800

    申请

---
+++ src/main/java/com/water/water/pojo/UserManage.java
@@ -0,0 +1,103 @@
+package com.water.water.pojo;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class UserManage {
+    private String UserID;
+    private String UserName;
+    private String UserPswd;
+    private String UClassID;
+    private Integer Ustate;
+    private Date RegTime;
+    private String MoPhone;
+    private String RealName;
+    private String DPTName;
+
+    public UserManage() {
+    }
+
+    public UserManage(String userID, String userName, String userPswd, String UClassID, Integer ustate, Date regTime, String moPhone, String realName, String DPTName) {
+        UserID = userID;
+        UserName = userName;
+        UserPswd = userPswd;
+        this.UClassID = UClassID;
+        Ustate = ustate;
+        RegTime = regTime;
+        MoPhone = moPhone;
+        RealName = realName;
+        this.DPTName = DPTName;
+    }
+
+    public String getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(String userID) {
+        UserID = userID;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserPswd() {
+        return UserPswd;
+    }
+
+    public void setUserPswd(String userPswd) {
+        UserPswd = userPswd;
+    }
+
+    public String getUClassID() {
+        return UClassID;
+    }
+
+    public void setUClassID(String UClassID) {
+        this.UClassID = UClassID;
+    }
+
+    public Integer getUstate() {
+        return Ustate;
+    }
+
+    public void setUstate(Integer ustate) {
+        Ustate = ustate;
+    }
+
+    public Date getRegTime() {
+        return RegTime;
+    }
+
+    public void setRegTime(Date regTime) {
+        RegTime = regTime;
+    }
+
+    public String getMoPhone() {
+        return MoPhone;
+    }
+
+    public void setMoPhone(String moPhone) {
+        MoPhone = moPhone;
+    }
+
+    public String getRealName() {
+        return RealName;
+    }
+
+    public void setRealName(String realName) {
+        RealName = realName;
+    }
+
+    public String getDPTName() {
+        return DPTName;
+    }
+
+    public void setDPTName(String DPTName) {
+        this.DPTName = DPTName;
+    }
+}
--- src/main/java/com/water/water/service/UserService.java
+++ src/main/java/com/water/water/service/UserService.java
@@ -1,9 +1,12 @@
 package com.water.water.service;
 
 import com.water.water.pojo.User;
+import com.water.water.pojo.UserManage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.water.water.dao.UserDao;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -14,6 +17,7 @@ public class UserService {
     public User getByusername(String username){
         return userDao.getByusername(username);
     }
+
     public User insertToUser(User user){
         userDao.insertToUser(user);
         return user;

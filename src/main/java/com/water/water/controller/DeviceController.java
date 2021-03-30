b108a393fcb7a2614bcb48cfd103b62712ccc6bd
MinghanHhh <72732534+MinghanHhh@users.noreply.github.com>
2021-03-30 16:15:29 +0800

    '第二次提交'

--- src/main/java/com/water/water/controller/DeviceController.java
+++ src/main/java/com/water/water/controller/DeviceController.java
@@ -20,20 +20,15 @@ public class DeviceController {
 //    获取全部控制柜
     @RequestMapping(value = "api/getAllTmnList")
     public List getAllTmnList(){
-//        System.out.println("获取Terminal表信息");
         List getAllTmnList = terminalsService.getAllTmnList();
-//        System.out.println(getAllTmnList);
         return getAllTmnList;
     }
 
 //    添加一个控制柜
     @PostMapping(value = "api/addTmn")
     public Result addTmn(@RequestBody Terminals requestTmn){
-        System.out.println(requestTmn);
-        System.out.println("增加一个Terminal");
         try {
             Integer result = terminalsService.addTmn(requestTmn);
-            System.out.println(result);
             if (result == 1){
                 return new Result(200);
             } else {
@@ -47,12 +42,9 @@ public class DeviceController {
 //    删除一个控制柜
     @PostMapping(value = "api/deleteTmn")
     public Result deleteTmn(@RequestBody Terminals tmn) {
-        System.out.println("删除这个控制柜"+tmn);
         String tmnID = tmn.getTmnId();
-        System.out.println(tmnID);
         try {
             Integer result = terminalsService.deleteTmn(tmnID);
-            System.out.println(result);
             if (result == 1) {
                 return new Result(200);
             } else {
@@ -66,19 +58,16 @@ public class DeviceController {
 //    修改控制柜信息
     @PostMapping(value = "api/modifyTmn")
     public Result modifyTmn(@RequestBody Terminals tmn) {
-        System.out.println("想要更新的控制柜信息"+tmn);
-        System.out.println(tmn.getTmnId());
-//        try {
+        try {
             Integer result = terminalsService.modifyTmn(tmn);
-            System.out.println(result);
             if (result == 1) {
                 return new Result(200);
             } else {
                 return new Result(400);
             }
-//        } catch (Exception e) {
-//            return new Result(400);
-//        }
+        } catch (Exception e) {
+            return new Result(400);
+        }
     }
 
 
